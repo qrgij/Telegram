9e6fbc3e91be802ced9120ac6055fe00858fab3b
qrgij <yi2262397@gmail.com>
2026-09-12 11:42:15 +0800

    Add Aurelia功能 entry in settings page

--- TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
+++ TMessagesProj/src/main/java/org/telegram/ui/SettingsActivity.java
@@ -697,6 +697,7 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
         items.add(SettingCell.Factory.of(8, IconBackgroundColors.CYAN.top, IconBackgroundColors.CYAN.bottom, R.drawable.settings_devices, getString(R.string.SettingsDevices), getString(R.string.SettingsDevicesInfo)));
         items.add(SettingCell.Factory.of(9, IconBackgroundColors.ORANGE_DEEP.top, IconBackgroundColors.ORANGE_DEEP.bottom, R.drawable.settings_power, getString(R.string.SettingsPowerSaving), getString(R.string.SettingsPowerSavingInfo)));
         items.add(SettingCell.Factory.of(10, IconBackgroundColors.PURPLE.top, IconBackgroundColors.PURPLE.bottom, R.drawable.settings_language, getString(R.string.SettingsLanguage), LocaleController.getCurrentLanguageName()));
+        items.add(SettingCell.Factory.of(100, 0xFFFF6B9D, 0xFFFF4081, R.drawable.settings_premium, "Aurelia功能", "防撤回、下载加速等"));
 
         items.add(UItem.asShadow(null));
 
@@ -835,6 +836,9 @@ public class SettingsActivity extends BaseFragment implements NotificationCenter
                 break;
             case 10:
                 presentSettingFragment(new LanguageSelectActivity());
+                break;
+            case 100:
+                presentSettingFragment(new org.telegram.messenger.AureliaSettingsActivity());
                 break;
 
             case 11:
